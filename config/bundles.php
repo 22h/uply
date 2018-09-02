d2a06699a5f38706c7332eb643835f4c536df521
Magnus Reiß <info@magnus-reiss.de>
2018-09-02 12:01:39 +0200

    add sentry for better exception handling management

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
